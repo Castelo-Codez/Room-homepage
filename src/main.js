a6b5f7450f836901fdbdb0e5c688666f2267557e
castelo <castlogomaa459@gmail.com>
2024-02-26 18:16:30 +0200

    second commit

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import "./base.scss";
 const $mobileNavOpener = document.querySelector("#mobile-nav-opener");
 const $mobileNavCloser = document.querySelector("#mobile-nav-closer");
 const $mobileNavContainer = document.querySelector("header > .container > div");
-
+const $nextButton = document.querySelector("#next-btn");
+const $previousButtin = document.querySelector("#previous-btn");
+const $lis = document.querySelectorAll("#images-list > li");
+const $articles = document.querySelectorAll("#features > article");
 $mobileNavOpener.addEventListener("click", (e) => {
     $openNav(true);
     e.stopPropagation();
@@ -27,3 +30,32 @@ $mobileNavContainer.querySelector("nav").addEventListener("click", (e) => {
 window.addEventListener("click", (e) => {
     $openNav(false);
 });
+
+let $currentIndex = 1;
+
+$nextButton.addEventListener("click", (e) => {
+    $currentIndex++;
+    $change();
+});
+
+$previousButtin.addEventListener("click", (e) => {
+    $currentIndex--;
+    $change();
+});
+
+function $change() {
+    if ($currentIndex < 1) {
+        $currentIndex = $lis.length;
+    }
+    if ($currentIndex > $lis.length) {
+        $currentIndex = 1;
+    }
+
+    $lis.forEach(($li) => {
+        console.log($currentIndex);
+        $lis.forEach((li) => li.classList.remove("active"));
+        $articles.forEach(($article) => $article.classList.remove("active"));
+        $lis[$currentIndex - 1].classList.add("active");
+        $articles[$currentIndex - 1].classList.add("active");
+    });
+}
